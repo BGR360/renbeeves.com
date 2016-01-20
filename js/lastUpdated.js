c61bb01d110c1acb137b34c1dd2f2eee34ccd085
Ben Reeves <benwolverine2019@gmail.com>
2016-01-20 01:23:30 -0500

    Implemented the JavaScript script to change the "last updated" date

--- js/lastUpdated.js
+++ js/lastUpdated.js
@@ -9,6 +9,16 @@ var MONTHS = [
 
 // Get the last updated date from the git repository
 var updatedDate = new Date();
+var github = new Github({
+    token: "007dc1733bcc150f11b93cb7f50651cb96864cc8",
+    auth: "oauth"
+});
+var repo = github.getRepo("BGR360", "BGR360.github.io");
+var lastUpdatedDateAsStr = "";
+repo.show(function(err, res){
+    lastUpdatedDateAsStr = res.updated_at;
+    updatedDate = new Date(lastUpdatedDateAsStr);
+});
 
 // Set the text in the footer
 var lastUpdatedSpan = document.getElementById("span_last_updated");
