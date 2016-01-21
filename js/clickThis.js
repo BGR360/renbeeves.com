08743a0aace076ad5a32500d71f46329a3e574af
Ben Reeves <benwolverine2019@gmail.com>
2016-01-21 16:10:08 -0500

    Added a super fun button

---
+++ js/clickThis.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Ben on 1/21/16.
+ */
+
+var responses = [
+    "I guess not.",
+    "You clicked it again.",
+    "Yo, give it up",
+    "Bruh.",
+    "Come on now.",
+    "There's nothing here for you.",
+    "CAN'T STUMP THE TRUMP!",
+    "hello world",
+    "javascript(exec:button-green)",
+    "^ That wasn't real code.",
+    "The tractor is red.",
+    "Click this??",
+    "You thought it was done.",
+    "You were wrong.",
+    "3",
+    "2",
+    "1",
+    "Blast Off!!!",
+    "Butterball",
+    "You must be bored.",
+    "I sure am bored.",
+    "Think I'll be done now.",
+    "kbye"
+];
+
+var currentResponse = 0;
+
+function onClickThis() {
+    var button = document.getElementById("button_click_this");
+    button.innerHTML = responses[currentResponse++];
+    if (currentResponse >= responses.length) {
+        currentResponse = 0;
+    }
+}
